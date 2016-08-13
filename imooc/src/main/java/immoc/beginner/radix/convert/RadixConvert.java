bc8d301f713a335dd55853ad40655903ef95468f
heben <heben99@163.com>
2016-08-13 19:55:01 +0800

    radix convert

---
+++ imooc/src/main/java/immoc/beginner/radix/convert/RadixConvert.java
@@ -0,0 +1,23 @@
+package immoc.beginner.radix.convert;
+
+public class RadixConvert {
+	public static void main(String[] args) {
+		//十进制转其他
+		int x = 15;
+		System.out.println(Integer.toHexString(x));
+		System.out.println(Integer.toOctalString(x));
+		System.out.println(Integer.toBinaryString(x));
+		
+		//其他转十进制
+		System.out.println(Integer.valueOf("FFFF", 16));
+		System.out.println(Integer.valueOf("17", 8));
+		System.out.println(Integer.valueOf("1001", 2));
+		
+		//异常处理
+		try {
+			System.out.println(Integer.valueOf("FFF",2));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
